8056542fb174bd778ae58fb83e250e01a5481af1
igorcamargo1 <rm94553@fiap.com.br>
2023-10-16 09:31:21 -0300

    first commit

---
+++ src/main/java/br/com/fiap/hospitalconsulta/HospitalconsultaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hospitalconsulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalconsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalconsultaApplication.class, args);
+	}
+
+}
